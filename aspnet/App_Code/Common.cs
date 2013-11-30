28656b4fe872b13d04659684f5caa0cc912740a8
dengwei <250100785@qq.com>
2013-11-30 18:58:56 +0800

    FlexPaper_2.1.2  php版本研究

---
+++ aspnet/App_Code/Common.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace lib
+{
+    public static class Common
+    {
+        public static Boolean validPdfParams(String pdfFilePath, String doc, String page)
+        {
+            return doc!=null  && !(doc.Length > 255 || page!=null && page.Length > 255);
+        }
+
+        public static Boolean validSwfParams(String swfFilePath, String doc, String page)
+        {
+            return doc != null && !(doc.Length > 255 || page != null && page.Length > 255);
+        }
+
+        public static int getTotalPages(string fileName)
+        {
+            using (StreamReader sr = new StreamReader(File.OpenRead(fileName)))
+            {
+                Regex regex = new Regex(@"/Type\s*/Page[^s]");
+                MatchCollection matches = regex.Matches(sr.ReadToEnd());
+
+                return matches.Count;
+            }
+        }
+
+        public static void setCacheHeaders(HttpContext context)
+        {
+            context.Response.AddHeader("Cache-Control", "private, max-age=10800, pre-check=10800");
+            context.Response.AddHeader("Pragma", "private");
+            context.Response.Cache.SetExpires(DateTime.Now.AddDays(2));
+        }
+
+    }
+}
---
+++ aspnet/Default.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using lib;
+using System.IO;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected Config configManager;
+    protected String loginerr;
+    protected String pdfFilePath;
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        configManager = new Config(Server.MapPath(VirtualPathUtility.GetDirectory(Request.Path)));
+
+        if (configManager.getConfig("admin.password") == null)
+        {
+            Response.Redirect("setup.aspx");
+            Response.End();
+        }
+
+        pdfFilePath = configManager.getConfig("path.pdf");
+
+        if(Request["pdfFile"]!=null){
+            String[] vals = Request["pdfFile"].Split(',');
+            for (int i = 0; i < vals.Length; i++)
+            {
+                if(File.Exists(pdfFilePath+vals[i])){
+                    File.Delete(pdfFilePath+vals[i]);
+                }
+            }
+        }
+
+        if (Request["ADMIN_USERNAME"] != null)
+        {
+            if(configManager.getConfig("admin.username") == Request["ADMIN_USERNAME"] &&
+               configManager.getConfig("admin.password") == Request["ADMIN_PASSWORD"]){
+                Session["FLEXPAPER_AUTH"] = "1";
+            }else{
+                Session["FLEXPAPER_AUTH"] = null;
+                loginerr = "Authentication failed. Please contact your system administrator for assistance.";
+            }
+        }
+    }
+}
